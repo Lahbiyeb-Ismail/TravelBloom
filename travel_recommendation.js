431f09be1c8b3b3c561a156e899cbf2b328635b5
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-05-13 17:25:00 +0100

    imporove search functionality

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,7 +1,7 @@
 const searchForm = document.getElementById('search_form');
 const searchInput = document.getElementById('search_input');
 const clearFormButton = document.getElementById('clear_form_button');
-const searchType = document.getElementById('search_type');
+// const searchType = document.getElementById('search_type');
 const recommendationSearch = document.getElementById('recommendation_search');
 
 async function fetchData() {
@@ -16,89 +16,86 @@ async function fetchData() {
   }
 }
 
-async function getSearchValue() {
+async function getSearchResults() {
   let searchResults = [];
   const searchValue = searchInput.value.toLowerCase().trim();
   const data = await fetchData();
-  const searchTypeValue = searchType.value.toLowerCase();
 
-  if (searchTypeValue === 'countries') {
-    searchResults = data?.countries?.filter((country) => {
-      if (
-        searchValue === country?.name?.toLowerCase() ||
-        searchValue === country?.cities?.name?.toLowerCase() ||
-        country?.cities?.description?.toLowerCase().includes(searchValue)
-      ) {
-        return country;
-      }
+  const dataKeys = Object.keys(data);
+
+  const dataKey = dataKeys.filter((key) => {
+    if (searchValue === key || key.toLowerCase().includes(searchValue)) {
+      return key;
+    }
+  });
+
+  if (dataKey.length > 0) {
+    searchResults = data[dataKey];
+    return searchResults;
+  } else {
+    const dataValues = Object.values(data);
+
+    searchResults = dataValues.map((value) => {
+      let results = value.filter((place) => {
+        if (
+          searchValue === place?.name?.toLowerCase() ||
+          place?.description?.toLowerCase().includes(searchValue) ||
+          place?.name?.toLowerCase().includes(searchValue)
+        ) {
+          return place;
+        }
+      });
+
+      return results;
     });
-  }
 
-  if (searchTypeValue === 'temples') {
-    searchResults = data?.temples?.filter((temple) => {
-      if (
-        temple.name.toLowerCase().includes(searchValue) ||
-        temple.description.toLowerCase().includes(searchValue) ||
-        searchValue === temple.name.toLowerCase()
-      ) {
-        return temple;
-      }
+    searchResults = searchResults.filter((result) => {
+      return result.length > 0;
     });
-  }
 
-  if (searchTypeValue === 'beaches') {
-    searchResults = data?.beaches?.filter((beach) => {
-      if (
-        beach.name.toLowerCase().includes(searchValue) ||
-        searchValue === beach.name.toLowerCase() ||
-        beach.description.toLowerCase().includes(searchValue)
-      ) {
-        return beach;
-      }
-    });
+    return searchResults[0];
   }
+}
 
-  return searchResults;
+function htmlTemplate(data) {
+  return `
+        <div class="destination">
+            <div class="destination_img">
+                <img src=${data.imageUrl} alt=${data.name}>
+            </div>
+            <div class="destination_info">
+                <h3 class="name">${data.name}</h3>
+                <p class="description">
+                    ${data.description}
+                </p>
+                <button type="button">Visit</button>
+            </div>
+        </div>
+        `;
 }
 
 function generateHtmlTemplate(data) {
-  const template = `
-        <div class="destination_img">
-            <img src=${data.imageUrl} alt=${data.name}>
-        </div>
-        <div class="destination_info">
-            <h3 class="name">${data.name}</h3>
-            <p class="description">
-                ${data.description}
-            </p>
-            <button type="button">Visit</button>
-        </div>
-    `;
+  if (data?.cities && data?.cities.length > 1) {
+    const cities = data.cities.map((city) => {
+      return htmlTemplate(city);
+    });
 
-  return template;
+    return cities.join('');
+  } else {
+    return htmlTemplate(data);
+  }
 }
 
 async function handleFormSubmit(e) {
   e.preventDefault();
-  const searchTypeValue = searchType.value.toLowerCase();
 
-  const searchResults = await getSearchValue();
-
-  console.log(searchResults);
+  const searchResults = await getSearchResults();
 
   let resultsHTML = searchResults
     ?.map((result) => {
-      if (searchTypeValue === 'countries') {
-        return generateHtmlTemplate(result.cities[0]);
-      }
+      if (result) return generateHtmlTemplate(result);
 
-      if (searchTypeValue === 'temples') {
-        return generateHtmlTemplate(result);
-      }
-
-      if (searchTypeValue === 'beaches') {
-        return generateHtmlTemplate(result);
-      }
+      return 'Noting found!';
     })
     .join('');
 
